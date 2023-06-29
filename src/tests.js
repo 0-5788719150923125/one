46a17c3959105dc028694ab1e89c57c3b66b7754
Luciferian Ink <LuciferianInk@protonmail.com>
2023-06-29 18:52:49 -0500

    add weight averaging functions

---
+++ src/tests.js
@@ -0,0 +1,88 @@
+import fs from 'node:fs'
+import { Recurrent, layer, utilities } from 'brain.js'
+import { TrainStream } from 'train-stream'
+import { mergeGRUNetworks } from './utils.js'
+const { add, input, multiply, output, random, rnnCell, lstmCell } = layer
+
+const myNet = JSON.parse(fs.readFileSync('./networks/compressor.28.json'))
+const urBit = JSON.parse(fs.readFileSync('./networks/compressor.29.json'))
+
+const ourPi = mergeGRUNetworks(myNet, urBit)
+
+for (let i = 0; i < 4; i++) {
+    console.log('checking neuron ' + i.toString())
+    console.log('myNet')
+    console.log(myNet.hiddenLayers[0].updateGateInputMatrix.weights[i])
+    console.log('urBit')
+    console.log(urBit.hiddenLayers[0].updateGateInputMatrix.weights[i])
+    console.log('averaged')
+    console.log(ourPi.hiddenLayers[0].updateGateInputMatrix.weights[i])
+}
+
+fs.writeFileSync('./networks/depressed.json', JSON.stringify(ourPi))
+
+// const net = new Recurrent({
+//     inputLayer: () => input({ width: 1, height: 24 }),
+//     hiddenLayers: [
+//         (input, recurrentInput) =>
+//             lstmCell({ height: 3 }, input, recurrentInput)
+//     ],
+//     outputLayer: (input) => output({ width: 1, height: 1 }, input)
+// })
+// const xor = [
+//     ['do, a deer, a female deer'],
+//     ['rae, a drop of golden sun'],
+//     ['mi, a name I call myself'],
+//     ['fa, a long long way to run']
+// ]
+
+// net.train(xor, {
+//     errorThresh: 0.023,
+//     iterations: 1,
+//     learningRate: 0.01,
+//     log: (details) => console.log(details),
+//     logPeriod: 10
+// })
+
+// console.log(net.run([0, 1]))
+
+// const trainingStream = new TrainStream({
+//     neuralNetwork: net,
+//     /**
+//      * Write training data to the stream. Called on each training iteration.
+//      */
+//     floodCallback: function () {
+//         readInputs(xor)
+//     },
+
+//     /**
+//      * Called when the network is done training.
+//      */
+//     doneTrainingCallback: function (stats) {
+//         console.log(
+//             `trained in ${stats.iterations} iterations with error: ${stats.error}`
+//         )
+
+//         const result01 = net.run([0, 1])
+//         const result00 = net.run([0, 0])
+//         const result11 = net.run([1, 1])
+//         const result10 = net.run([1, 0])
+
+//         console.log('0 XOR 1: ', result01) // 0.987
+//         console.log('0 XOR 0: ', result00) // 0.058
+//         console.log('1 XOR 1: ', result11) // 0.087
+//         console.log('1 XOR 0: ', result10) // 0.934
+//     }
+// })
+
+// // kick it off
+// readInputs(xor)
+
+// function readInputs(data) {
+//     for (let i = 0; i < data.length; i++) {
+//         trainingStream.write(data[i])
+//     }
+
+//     // let it know we've reached the end of the inputs
+//     trainingStream.endInputs()
+// }
--- src/utils.js
+++ src/utils.js
@@ -31,3 +31,118 @@ export function getRandomIdentity() {
     }
     return randomNumber
 }
+
+export function averageArrays(arr1, arr2) {
+    const maxLength = Math.max(arr1.length, arr2.length)
+    const averagedArray = []
+
+    for (let i = 0; i < maxLength; i++) {
+        const value1 = arr1[i] !== undefined ? arr1[i] : arr2[i]
+        const value2 = arr2[i] !== undefined ? arr2[i] : arr1[i]
+        const average = (value1 + value2) / 2
+        averagedArray.push(average)
+    }
+
+    return averagedArray
+}
+
+export function mergeGRUNetworks(myNet, urBit) {
+    return {
+        type: myNet.type,
+        options: {
+            inputSize: myNet.options.inputSize,
+            inputRange: myNet.options.inputRange,
+            hiddenLayers: myNet.options.hiddenLayers,
+            outputSize: myNet.options.outputSize,
+            decayRate: myNet.options.decayRate,
+            smoothEps: myNet.options.smoothEps,
+            regc: myNet.options.regc,
+            clipval: myNet.options.clipval,
+            maxPredictionLength: myNet.options.maxPredictionLength,
+            dataFormatter: {
+                indexTable: myNet.options.dataFormatter.indexTable,
+                characterTable: myNet.options.dataFormatter.characterTable,
+                values: myNet.options.dataFormatter.values,
+                characters: myNet.options.dataFormatter.characters,
+                specialIndexes: myNet.options.dataFormatter.specialIndexes
+            }
+        },
+        learningRate: myNet.learningRate,
+        errorThresh: myNet.errorThresh,
+        trainOpts: {
+            iterations: myNet.trainOpts.iterations,
+            errorThresh: myNet.trainOpts.errorThresh,
+            log: myNet.trainOpts.log,
+            logPeriod: myNet.trainOpts.logPeriod,
+            learningRate: myNet.trainOpts.learningRate,
+            callbackPeriod: myNet.trainOpts.callbackPeriod,
+            timeout: myNet.trainOpts.timeout
+        },
+        input: {
+            rows: myNet.input.rows,
+            columns: myNet.input.columns,
+            weights: averageArrays(
+                myNet.input.weights,
+                urBit.input.weights
+            ).slice(0, myNet.input.rows * myNet.input.columns)
+        },
+        hiddenLayers: mergeHiddenLayers(myNet, urBit),
+        outputConnector: {
+            rows: myNet.outputConnector.rows,
+            columns: myNet.outputConnector.columns,
+            weights: averageArrays(
+                myNet.outputConnector.weights,
+                urBit.outputConnector.weights
+            ).slice(
+                0,
+                myNet.outputConnector.rows * myNet.outputConnector.columns
+            )
+        },
+        output: {
+            rows: myNet.output.rows,
+            columns: myNet.output.columns,
+            weights: averageArrays(
+                myNet.output.weights,
+                urBit.output.weights
+            ).slice(0, myNet.output.rows * myNet.output.columns)
+        }
+    }
+}
+
+function mergeHiddenLayers(myNet, urBit) {
+    const keys = [
+        'updateGateInputMatrix',
+        'updateGateHiddenMatrix',
+        'updateGateBias',
+        'resetGateInputMatrix',
+        'resetGateHiddenMatrix',
+        'resetGateBias',
+        'cellWriteInputMatrix',
+        'cellWriteHiddenMatrix',
+        'cellWriteBias'
+    ]
+    const hiddenLayers = []
+    try {
+        for (let i = 0; i < myNet.hiddenLayers.length; i++) {
+            const layer = {}
+            for (const key of keys) {
+                layer[key] = {
+                    rows: myNet.hiddenLayers[i][key].rows,
+                    columns: myNet.hiddenLayers[i][key].columns,
+                    weights: averageArrays(
+                        myNet.hiddenLayers[i][key].weights,
+                        urBit.hiddenLayers[i][key].weights
+                    ).slice(
+                        0,
+                        myNet.hiddenLayers[i][key].rows *
+                            myNet.hiddenLayers[i][key].columns
+                    )
+                }
+            }
+            hiddenLayers.push(layer)
+        }
+    } catch {
+        // pass
+    }
+    return hiddenLayers
+}
